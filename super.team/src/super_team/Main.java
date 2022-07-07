2137844b5bca2d2c8954583f43768e8377e73b04
Nurmuhammad <nur.nurbekov.01@gmail.com>
2022-07-07 11:51:20 +0600

    I changed

--- super.team/src/super_team/Main.java
+++ super.team/src/super_team/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static <String> void main(String[] args) {
         int a = 0 ;
         System.out.println("Hllo Nurbekov");
-
-
+        System.out.println("Hello Aziza");
     }
 }
