df2f06c5831cdaf2c325867f28b4dccb39df9607
Aruuk <skybanychnek@gmail.com>
2022-07-07 14:39:20 +0600

    1 stage change

--- super.team/src/super_team/Main.java
+++ super.team/src/super_team/Main.java
@@ -7,19 +7,21 @@ import java.util.Arrays;
 
 public class Main {
     public static final GsonBuilder GSON_BUILDER = new GsonBuilder();
-
-
+    public static final Gson GSON = GSON_BUILDER.setPrettyPrinting().create();
     public static void main(String[] args) {
         Lorry [] lorries = {
                 Lorry.makeLorry(1,"Volvo", "Jake", State.BASE),
                 Lorry.makeLorry(2, "Renault", "Tom", State.BASE),
                 Lorry.makeLorry(3, "DAF XT", "Kim", State.BASE)
         };
+        String json = GSON.toJson(lorries);
 
         System.out.println("#  | Bus       | Driver   | State ");
         System.out.println("---|-----------|----------|--------");
         Arrays.stream(lorries).filter(x ->x.getId() == 1).forEach(System.out::println);
         Arrays.stream(lorries).filter(x -> x.getId() == 2).forEach(System.out::println);
         Arrays.stream(lorries).filter(x -> x.getId() == 3).forEach(System.out::println);
+
+        System.out.println(json);
     }
     }
